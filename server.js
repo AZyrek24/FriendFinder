a8e278309e28ba5051cfbb27084cf3e4c689bb9b
AZyrek24 <zzeppelin2400@gmail.com>
2018-07-25 12:21:12 -0700

    html routing, api routing working

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.get("/", function(err, res) {
-  res.send("YOYOYOYOYO");
-})
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
+app.use(express.static('./app/public'));
 // Server listener
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT)
