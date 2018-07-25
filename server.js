8037f5d656fe66b27e39abd35a7673181a821666
AZyrek24 <zzeppelin2400@gmail.com>
2018-07-25 12:38:29 -0700

    fixed static image for background

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ var PORT = process.env.PORT || 8080;
 // Parse application
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+app.use(express.static('./app/public'));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.use(express.static('./app/public'));
 // Server listener
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT)
