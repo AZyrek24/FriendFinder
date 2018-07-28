d1e07f7042849b3aaf43b47c24d092707fee454f
AZyrek24 <zzeppelin2400@gmail.com>
2018-07-28 16:32:38 -0700

    apiRoutes checkScore function added, lots of little fixes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,50 @@
 var friends = [
   {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Borat",
+    "photo": "http://ichef.bbci.co.uk/wwfeatures/wm/live/1280_640/images/live/p0/4d/gk/p04dgkm4.jpg",
     "scores": [
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    "name": "Joker",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkPOQvUH-G5fJji11e9YQnIZ7hXjx3fgJ6sQjx_P8zIbSmtujckQ",
+    "scores": [
+      "3",
+      "5",
+      "2",
+      "1",
+      "3",
+      "2",
+      "5",
+      "2",
+      "3",
+      "3"
+    ]
+  },
+  {
+    "name": "Gargamel",
+    "photo": "https://i.ytimg.com/vi/ukBtM6Jo630/hqdefault.jpg",
+    "scores": [
+      "2",
+      "4",
+      "1",
+      "1",
+      "5",
+      "5",
+      "1",
+      "4",
+      "2",
+      "3"
     ]
   }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,35 @@
-var friendsData = require('../data/friends.js');
-
+var friendsData = require('../data/friends');
+var newFriend = [];
+var lowestScoreDiff = 1000;
+var foundFriend;
 module.exports = function(app) {
   app.get('/api/friends', function (req, res) {
     res.json(friendsData);
   });
+
+  app.post('/api/friends', function (req, res) {
+    newFriend = req.body;
+    friendsData.push(req.body);
+    checkScores();
+    console.log("NEW FRIEND: ")
+    console.log(foundFriend);
+  })
+}
+
+function checkScores() {
+  foundFriend = friendsData[0];
+  for (var i = 0; i < friendsData.length - 1; i++) {
+   
+    var differenceNum = 0;
+    for (var j = 0; j < 10; j++) {
+      differenceNum += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriend.scores[j])));
+    }
+    console.log(friendsData[i].name);
+    console.log(differenceNum);
+    if (differenceNum < lowestScoreDiff) {
+      lowestScoreDiff = differenceNum;
+      foundFriend = friendsData[i];
+    }    
+  }
+  return;
 }
